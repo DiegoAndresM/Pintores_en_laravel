08a89627666594736e060a91eed5ea9801e077ad
DiegoAndresM <65470307+DiegoAndresM@users.noreply.github.com>
2020-11-04 20:58:03 -0700

    Add files via upload

---
+++ app/Http/Controllers/Pintores.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Pintores extends Controller
+{
+    public function index(){
+        return view ('index');
+    }
+
+    public function leonardo(){
+        return view ('leonardo');
+    }
+
+    public function picasso(){
+        return view ('picasso');
+    }
+
+    public function monet(){
+        return view ('monet');
+    }
+
+    public function vincent(){
+        return view ('vincent');
+    }
+}
